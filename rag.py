8504c87afa44db14c6d63297e71e6e11d27d61bf
Rohil Pal <rohilpal9763@gmail.com>
2024-03-18 10:34:12 +0000

    Modify tasks breakdown prompt

--- rag.py
+++ rag.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 from openai import OpenAI
 import os
 import re
+import json
 
 os.environ['OPENAI_API_KEY'] = "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"
 
@@ -36,103 +37,128 @@ class AutomateRAG:
             7. Code correction
         """
 
-        print (f"Job description\n{'--'*50}\n{job_description}")
-        job_trigger, tasks = self.break_job_into_tasks(job_description=job_description)
+        print (f"Job description\n{'--'*50}\n{job_description}\n")
 
-        print (f"Job Trigger\n{'--'*50}\n{job_trigger}")
-        print (f"Tasks\n{'--'*50}\n{tasks}")
-        
-        integrations, integrations_output = self.identify_integrations(job_trigger=job_trigger, tasks=tasks)
+        self.tasks_and_trigger = self.break_job_into_tasks(job_description=job_description)
 
-        integrations = list(set(integrations))
-        print (f"Here is the list of integrations:\n{integrations}")
-        print (f"Here is the LLM output for integrations:\n{integrations_output}")
+        print (f"Tasks and Trigger:\n{'--'*50}\n{self.tasks_and_trigger}\n")
+
+        # integrations, integrations_output = self.identify_integrations(job_trigger=job_trigger, tasks=tasks)
+
+        # integrations = list(set(integrations))
+        # print (f"Here is the list of integrations:\n{integrations}")
+        # print (f"Here is the LLM output for integrations:\n{integrations_output}")
+
+        # ctx = job_trigger + tasks + integrations_output + f"{integrations}"
+        # examples = self.fetch_examples(ctx)
 
     def break_job_into_tasks(self, job_description: str) -> List[str]:
         """
         Given a job description, break down into one or more tasks
         """
-        TASKS_BREAKDOWN_PROMPT = f"""
+        TASKS_BREAKDOWN_PROMPT = """
         **Goal**
         You are given a job description. A Job is defined as a sequence of tasks and it can be triggered due to an event, webhook or a schedule
 
-        Your goal is to understand the job and break it down into a sequence of tasks.
+        Your goal is to understand the job and - (1) Identify the job trigger (2) Break down the job into a sequence of tasks.
 
-        **Guidelines to follow**
+        Strictly adhere to the output format. The output format must be a valid JSON and its schema is defined below under the **Output format** section.
+
+        Do not include any leading or trailing backticks, single quotes, double quotes or any other symbols in the output.
+
+        The output should always start with a { and end with a }
+
+        **Output format**
         
-        1. The first task should always be a trigger.
-        2. The subsequent tasks should be ordered.
-        3. Strictly adhere to the output format
+        {
+            "job_trigger": {
+                "type": <<trigger_type>>, // valid values are 'webhook', 'event', 'schedule'
+                "explanation": <<One line explanation for why this trigger type is chosen>>,
+                "params": <<Relevant trigger params>> // For example, for a 'schedule' trigger, it can be the actual schedule like 'Every Friday at 4 PM'.
+            },
 
+            "tasks": [
+                {   
+                    "task_sequence_id": 1
+                    "task_desc": <<Exactly one line concise and accurate description of the task>>
+                },
+                {
+                    "task_sequence_id": 2
+                    "task_desc": <<Exactly one line concise and accurate description of the task>>   
+                },
+                .
+                .
+                .
+                {
+                    "task_sequence_id": N
+                    "task_desc": <<Exactly one line concise and accurate description of the task>>   
+                }
+            ]
+        }
+
+        
         **Examples**
 
         Example 1:
         Job Description: Update Airtable when a new subscription is added to Stripe.
 
-        Tasks:
-        
-        Job trigger:
-            - webhook
-            - Trigger type is 'webhook' because job will listen to 'New subscription' events from Stripe API
-            - New subscription is added to Stripe database
-        
-        1. Add a new record containing the new subscription details in Airtable.
+        Output:
 
+        {
+            "job_trigger": {
+                "type": "webhook",
+                "explanation": "Trigger type is 'webhook' because job will listen to 'New subscription' events from Stripe API",
+                "params": "New subscription is added to Stripe database"
+            },
+
+            "tasks": [
+                {   
+                    "task_sequence_id": 1,
+                    "task_desc": "Add a new record containing the new subscription details in Airtable."
+                }
+            ]
+        }        
+        
         Example 2:
         Job Description: Send an activity summary email, and post it to Slack at 4PM every Friday.
 
-        Tasks:
+        Output:
 
-        Job trigger:
-            - schedule
-            - Trigger type is 'schedule' because job will trigger every Friday at 4 PM.
-            - Every Friday at 4 PM.
-        
-        1. Send a weekly summary email to users who have opted-in to receive weekly summaries.
-        2. Send a slack message to relevant channel informing that a weekly summary has been sent to the relevant users.
+        {
+            "job_trigger": {
+                "type": "schedule",
+                "explanation": "Trigger type is 'schedule' because job will trigger every Friday at 4 PM.",
+                "params": "Every Friday at 4 PM."
+            },
+
+            "tasks": [
+                {   
+                    "task_sequence_id": 1,
+                    "task_desc": "Send a weekly summary email to users who have opted-in to receive weekly summaries."
+                },
+                {
+                    "task_sequence_id": 2,
+                    "task_desc": "Send a slack message to relevant channel informing that a weekly summary has been sent to the relevant users."
+                }
+            ]
+        }
 
         **Job Description**
-        {job_description}
-        
-        **Output format**
-        
-        Tasks:
-
-        Job trigger: 
-            - <<trigger_type>> // valid values are 'webhook', 'event', 'schedule'
-            - <<One line explanation for why this trigger type is chosen>>
-            - Relevant trigger params // For example, for a 'schedule' trigger, it can be the actual schedule like 'Every Friday at 4 PM'.
-        1. Task 1
-        2. Task 2
-            .
-            .
-            .
-        N. Task N
-
         """
         
-        tasks_breakdown_output = self.__invoke_llm_api(llm_name=self.main_llm, query=TASKS_BREAKDOWN_PROMPT)
+        prompt = TASKS_BREAKDOWN_PROMPT + f"\n{job_description}" + "\n\nOutput:"
+        
+        output = self.__invoke_llm_api(llm_name=self.main_llm, query=prompt)
 
-        job_trigger, tasks = self.__parse_tasks_and_trigger(input_text=tasks_breakdown_output)
+        tasks_and_trigger = self.__parse_tasks_and_trigger(input_text=output)
 
-        return job_trigger, tasks
-
+        return tasks_and_trigger
 
 
     def __parse_tasks_and_trigger(self, input_text):
-        # Find the start of the Job trigger section
-        trigger_start_index = input_text.find("Job trigger:")
-
-        # Assuming the tasks always start with "1.", find the start of the tasks
-        tasks_start_index = input_text.find("1.")
+        json_data = json.loads(input_text)
         
-        # Extract the Job trigger section
-        job_trigger_section = input_text[trigger_start_index:tasks_start_index].strip()
-        
-        # Extract the tasks section
-        tasks_section = input_text[tasks_start_index:].strip()
-        
-        return job_trigger_section, tasks_section
+        return json_data
 
     def identify_integrations(self, job_trigger: Dict, tasks: str) -> List[str]:
         """
@@ -196,12 +222,15 @@ class AutomateRAG:
         return matches
 
 
-    def fetch_examples(self, integrations: List[str]):
+    def fetch_examples(self, ctx):
         """
         This is where we will do retrieval from qdrant vector DB
-        We will fetch all examples for this integration
+        We will fetch all examples that are relevant to the job trigger, tasks and integrations.
         """
-        pass
+        FETCH_EXAMPLES_PROMPT = """
+        
+
+        """
 
     def __invoke_llm_api(self, llm_name, query, llm_provider='openai'):
 
